5ddd10153396f9544f40f51ea23e49c9cf79f56e
Shanky Kalra <shanky.kalra@ihsmarkit.com>
2021-08-03 19:53:59 +0530

    Adding PDF Extraction

---
+++ PDF_Work/Extract_PDF.py
@@ -0,0 +1,28 @@
+import PyPDF2
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PyPDF2.PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    txt = f"""
+    Information about {pdf_path}: 
+
+    Author: {information.author}
+    Creator: {information.creator}
+    Producer: {information.producer}
+    Subject: {information.subject}
+    Title: {information.title}
+    Number of pages: {number_of_pages}
+    """
+    print(txt)
+    return information
+
+        
+if __name__ == '__main__':
+    path = 'C:\VS_Code_Python_POC\Visual_Test\PDF_Work\Azure.pdf'
+    extract_information(path)
+
+
+#PyPDF2.PdfFileReader('Azure.pdf')
